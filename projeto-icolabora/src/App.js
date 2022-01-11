0cedd01cc086ec370446cf2ac5a8f3eb790dc205
Filipe <henriqaraujo1@gmail.com>
2022-01-11 17:21:05 -0300

    API está ok
    Api esta se comunicando corretamente e o front está recebendo as informações, agora é organizar e adicionar estilo

--- projeto-icolabora/src/App.js
+++ projeto-icolabora/src/App.js
@@ -1,88 +1,79 @@
-import React, { useEffect, useState} from "react";
+import React, {  useEffect, useState} from "react";
 import api from "./services/Api";
 
-// function Tabela() {
-//   const { dados} = this.props;
-//   const [open, setOpen] = useState(false);
 
-//   return (
-//     <React.Fragment>
-//       <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-//         <TableCell>
-//           <IconButton
-//             aria-label="expand row"
-//             size="small"
-//             onClick={() => setOpen(!open)}
-//           >
-//             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-//           </IconButton>
-//         </TableCell>
-//         <TableCell component="th" scope="row">
-//           {row.name}
-//         </TableCell>
-//         <TableCell align="right">{dados.DRILLDOWN.SLA}</TableCell>
-//         <TableCell align="right">{row.fat}</TableCell>
-//         <TableCell align="right">{row.carbs}</TableCell>
-//         <TableCell align="right">{row.protein}</TableCell>
-//       </TableRow>
-//       <TableRow>
-//         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-//           <Collapse in={open} timeout="auto" unmountOnExit>
-//             <Box sx={{ margin: 1 }}>
-//               <Typography variant="h6" gutterBottom component="div">
-//                 History
-//               </Typography>
-//               <Table size="small" aria-label="purchases">
-//                 <TableHead>
-//                   <TableRow>
-//                     <TableCell>Date</TableCell>
-//                     <TableCell>Customer</TableCell>
-//                     <TableCell align="right">Amount</TableCell>
-//                     <TableCell align="right">Total price ($)</TableCell>
-//                   </TableRow>
-//                 </TableHead>
-//                 <TableBody>
-//                   {row.history.map((historyRow) => (
-//                     <TableRow key={historyRow.date}>
-//                       <TableCell component="th" scope="row">
-//                         {historyRow.date}
-//                       </TableCell>
-//                       <TableCell>{historyRow.customerId}</TableCell>
-//                       <TableCell align="right">{historyRow.amount}</TableCell>
-//                       <TableCell align="right">
-//                         {Math.round(historyRow.amount * row.price * 100) / 100}
-//                       </TableCell>
-//                     </TableRow>
-//                   ))}
-//                 </TableBody>
-//               </Table>
-//             </Box>
-//           </Collapse>
-//         </TableCell>
-//       </TableRow>
-//     </React.Fragment>
-//   );
-// }
+
 
 export default function App() {
   const [atendimento, setAtendimento] = useState([]);
+  const [Sla, setSla] = useState([])
+
   useEffect(() => {
     api
     .get()
     .then((response) => {
-      console.log(response.data[0].DRILLDOWN[0])
-      setAtendimento(response.data.DRILLDOWN)})
+      console.log(response.data[0].DRILLDOWN[0].SLA)
+    
+      setAtendimento(response.data)})
+      
     .catch((err) => {
       console.error(`Erro na Conexão com a API ${err}`)
     })
   }, [])
 
 
+  
+
+
   return (
     <div>
-      {atendimento.map(dado => {
-        return <h1>{dado['DRILLDOWN']['0']['SLA']}</h1>
-      })}
+      <table>
+        <thead>
+          <tr>
+            <th>Tarefa</th>
+            <th>Vencido</th>
+            <th>D0</th>
+            <th>D1</th>
+            <th>D2</th>
+            <th>D3</th>
+            <th>Total</th>
+            <th>Detalhes</th>
+          </tr>
+        </thead>
+        <tbody>
+        
+            {atendimento.map((dado) => {
+              return(
+                <>
+                  <tr>
+                  <td 
+                  key={dado.TAREFA}>{dado.TAREFA}</td>
+                  <td key={dado.VENCIDO}>{dado.VENCIDO}</td>
+                  <td key={dado.D0}>{dado.D0}</td>
+                  <td key={dado.D1}>{dado.D1}</td>
+                  <td key={dado.D2}>{dado.D2}</td>
+                  <td key={dado.D3}>{dado.D3}</td>
+                  <td key={dado.TOTAL}>{dado.TOTAL}</td>
+                  <td>
+                    <button>
+                      Detalhes
+                    </button>
+                  </td>
+                </tr>
+                <>
+                  {dado.DRILLDOWN.map((sla) => {
+                    return <p 
+                    
+                    key={sla.SLA}>SLA: {sla.SLA}</p>
+                  })}
+                </>
+                </>
+                
+                
+              )
+            })}
+        </tbody>
+      </table>     
     </div>
     
     
