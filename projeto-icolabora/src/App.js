4be15c8a397f7beac7d455f0874b250c9f0d166d
Filipe <henriqaraujo1@gmail.com>
2022-01-12 16:14:17 -0300

    Teste finalizado

--- projeto-icolabora/src/App.js
+++ projeto-icolabora/src/App.js
@@ -6,85 +6,69 @@ export default function App() {
   const [atendimento, setAtendimento] = useState([]);
   const [isShow, setShow] = useState(false);
 
+  /*Click para mostrar os detalhes*/
   const handleClick = () => {
-    setShow(!isShow)
+    setShow(!isShow)     
   }
 
+
+  /*Chamada da API e passando para setAtendimento*/
   useEffect(() => {
     api
       .get()
-      .then((response) => {
-        console.log(response.data[0].DRILLDOWN[0].SLA)
-
-        setAtendimento(response.data)
-      })
-
+      .then((response) => setAtendimento(response.data))
       .catch((err) => {
         console.error(`Erro na Conexão com a API ${err}`)
       })
   }, [])
 
+
+  /*Construção da tabela*/
   return (
     <div>
       <table>
         <thead>
           <tr id="titulo">
             <th id="tarefa">Tarefa</th>
-            <th class="resultados">Vencido</th>
-            <th class="indicadores">D0</th>
-            <th class="indicadores">D1</th>
-            <th class="indicadores">D2</th>
-            <th class="indicadores">D3</th>
-            <th class="resultados">Total</th>
+            <th className="resultados">Vencido</th>
+            <th className="indicadores">D0</th>
+            <th className="indicadores">D1</th>
+            <th className="indicadores">D2</th>
+            <th className="indicadores">D3</th>
+            <th className="resultados">Total</th>
           </tr>
         </thead>
         <tbody>
-
+         {/*Usei um Map para percorrer o JSON e organizar dentro da tabela*/}
           {atendimento.map((dado) => {
             return (
               <>
-                <tr 
-                  id="result-celula"
-                  onClick={handleClick}>
-                  <td
-                    id="result-tarefa"
-                    key={dado.TAREFA}>{dado.TAREFA}</td>
-                  <td
-                    class="result-totais" 
-                    key={dado.VENCIDO}>{dado.VENCIDO}</td>
-                  <td 
-                  id="D0"
-                  key={dado.D0}>{dado.D0}</td>
-                  <td 
-                  id="D1"
-                  key={dado.D1}>{dado.D1}</td>
-                  <td 
-                  id="D2"
-                  key={dado.D2}>{dado.D2}</td>
-                  <td 
-                  id="D3"
-                  key={dado.D3}>{dado.D3}</td>
-                  <td 
-                  class="result-totais"
-                  key={dado.TOTAL}>{dado.TOTAL}</td>
+                <tr id="result-celula">
+                  <td onClick={handleClick} id="result-tarefa">{dado.TAREFA}</td>
+                  <td className="result-totais">{dado.VENCIDO}</td>
+                  <td id="D0">{dado.D0}</td>
+                  <td id="D1">{dado.D1}</td>
+                  <td id="D2">{dado.D2}</td>
+                  <td id="D3">{dado.D3}</td>
+                  <td className="result-totais">{dado.TOTAL}</td>
                 </tr>
+                {/*Para conseguir abrir os detalhes, criei uma Condição Ternaria,
+                  se for ao clicar na tarefa ira mudar isShow para True e mostra os detalhes, e false esconde
+                  os detalhes, porem não consegui deixar para mostrar os detalhes por tarefa
+                */}  
                 {isShow ?
                   <>
                     {dado.DRILLDOWN.map((sla) => {
                       return <tr>
-                          <td 
-                          id='info'
-                          key={sla.SLA}>SLA: {sla.SLA}</td>
+                        <td
+                          id='info'>SLA: {sla.SLA}</td>
                       </tr>
-
-                        
                     })}
                   </> : <></>}
 
               </>
             )
           })}
-
         </tbody>
       </table>
     </div>
