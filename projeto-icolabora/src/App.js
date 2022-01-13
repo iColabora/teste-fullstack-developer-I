66063cc904984dc46cf95038ebe20a78de55a708
Filipe <henriqaraujo1@gmail.com>
2022-01-13 17:31:09 -0300

    Correção do estilo da celular totalizatira

--- projeto-icolabora/src/App.js
+++ projeto-icolabora/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import api from "./services/Api";
 import './styles/styles.scss'
+import Collapsible from 'react-collapsible'
 
 export default function App() {
   const [atendimento, setAtendimento] = useState([]);
@@ -11,12 +12,23 @@ export default function App() {
     setShow(!isShow)     
   }
 
+  function Detalhes(props) {
+    const { details } = props;
+
+    return (
+      <tr>
+         <p>{details.SLA}</p>
+      </tr>
+     
+    )
+  }
+
 
   /*Chamada da API e passando para setAtendimento*/
   useEffect(() => {
     api
       .get()
-      .then((response) => setAtendimento(response.data))
+      .then((response) => {setAtendimento(response.data)})
       .catch((err) => {
         console.error(`Erro na Conexão com a API ${err}`)
       })
@@ -40,35 +52,32 @@ export default function App() {
         </thead>
         <tbody>
          {/*Usei um Map para percorrer o JSON e organizar dentro da tabela*/}
-          {atendimento.map((dado) => {
-            return (
-              <>
-                <tr id="result-celula">
-                  <td onClick={handleClick} id="result-tarefa">{dado.TAREFA}</td>
-                  <td className="result-totais">{dado.VENCIDO}</td>
-                  <td id="D0">{dado.D0}</td>
-                  <td id="D1">{dado.D1}</td>
-                  <td id="D2">{dado.D2}</td>
-                  <td id="D3">{dado.D3}</td>
-                  <td className="result-totais">{dado.TOTAL}</td>
-                </tr>
-                {/*Para conseguir abrir os detalhes, criei uma Condição Ternaria,
-                  se for ao clicar na tarefa ira mudar isShow para True e mostra os detalhes, e false esconde
-                  os detalhes, porem não consegui deixar para mostrar os detalhes por tarefa
-                */}  
-                {isShow ?
-                  <>
-                    {dado.DRILLDOWN.map((sla) => {
-                      return <tr>
-                        <td
-                          id='info'>SLA: {sla.SLA}</td>
-                      </tr>
-                    })}
-                  </> : <></>}
+          {atendimento.map((dado, id) => (
+            <>
+              <tr key={id} id={`result-${id+1}`} onClick={Detalhes}>
+                <td onClick={handleClick} id="result-tarefa">{dado.TAREFA}</td>
+                <td className="result-totais">{dado.VENCIDO}</td>
+                <td id="D0">{dado.D0}</td>
+                <td id="D1">{dado.D1}</td>
+                <td id="D2">{dado.D2}</td>
+                <td id="D3">{dado.D3}</td>
+                <td className="result-totais">{dado.TOTAL}</td>
+              </tr>
+              {/*Para conseguir abrir os detalhes, criei uma Condição Ternaria,
+      se for ao clicar na tarefa ira mudar isShow para True e mostra os detalhes, e false esconde
+      os detalhes, porem não consegui deixar para mostrar os detalhes por tarefa
+    */}
+              {isShow ?
+                <>
+                  {dado.DRILLDOWN.map((sla) => {
+                    return (
+                      <Detalhes details={sla} />
+                    )
+                  })}
+                </> : <></>}
 
-              </>
-            )
-          })}
+            </>
+          ))}
         </tbody>
       </table>
     </div>
