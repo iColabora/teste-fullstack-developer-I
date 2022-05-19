3341bf05ab998e63470cd6eaba15728acf549615
Cláudio Marcelo Ribeiro <claudio.ribeiro@lucree.com.br>
2022-05-19 00:33:21 -0300

    criação de rota

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app id="app">
+    <Template v-if="this.$router.history.current.meta.showMenu">
+      <router-view />
+    </Template>
+    <div class="div-login" v-else>
+      <router-view />
+    </div>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Template from './components/Template.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Template
+  },
+  computed: {
+    showMenu(){
+      return this.$router.history.current.meta.showMenu
+    }
   }
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+  .div-login {
+      flex: 1;
+      background: rgb(2, 0, 36);
+      background: linear-gradient(
+      180deg,
+      rgb(248, 227, 226) 0%,
+      rgb(255, 255, 255) 48%,
+      rgba(211, 242, 234) 100%
+    );
 }
 </style>
