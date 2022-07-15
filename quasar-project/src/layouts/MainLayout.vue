99770845fd9cb12fd75d41d22837f0ce6b85aebf
Paulo Eduardo Vizioli Garcia <pauloevg320@gmail.com>
2022-07-15 11:04:00 -0300

    Envio do Teste

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -1,7 +1,18 @@
 <template>
   <q-layout view="">
+    <Relatorio/>
   </q-layout>
 </template>
 
 <script>
+import Relatorio from '../pages/IndexPage.vue'
+export default {
+  name: 'MeuTeste',
+  components: {
+    Relatorio
+  },
+  data () {
+    return {}
+  }
+}
 </script>
--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,13 +1,93 @@
 <template>
   <q-page>
     <div class="row">
+      <h5>Relatório</h5>
+      <div class="col-12">
       <q-table
-      title="Relatório"
-      :rows="histograma"
-      :columns="columns"
-      row-key="tarefa"
-      class="col"
-    ></q-table>
+        :rows="histograma"
+        :columns="columns"
+        row-key="tarefa"
+        table-class="col-12"
+      >
+        <template #body="props">
+          <q-tr
+            :props="props"
+            :class="(props.row.TAREFA === 'Total')?'total text-white':''"
+          >
+            <q-td
+              key="tarefa"
+              :props="props"
+            >
+              {{ props.row.TAREFA }}
+            </q-td>
+
+            <q-td
+              key="vencido"
+              :props="props"
+              @click="mostrar(props.row.TAREFA, 'VENCIDO')"
+              :class="(props.row.TAREFA === 'Total')?'total text-white':'padrao'"
+            >
+              {{ props.row.VENCIDO }}
+            </q-td>
+
+            <q-td
+              key="d0"
+              :props="props"
+              @click="mostrar(props.row.TAREFA, 'D0')"
+              :class="(props.row.TAREFA === 'Total')?'total text-white':'d0'"
+            >
+              {{ props.row.D0 }}
+            </q-td>
+
+            <q-td
+              key="d1"
+              :props="props"
+              @click="mostrar(props.row.TAREFA, 'D1')"
+              :class="(props.row.TAREFA === 'Total')?'total text-white':'d1'"
+            >
+              {{ props.row.D1 }}
+            </q-td>
+
+            <q-td
+              key="d2"
+              :props="props"
+              @click="mostrar(props.row.TAREFA, 'D2')"
+              :class="(props.row.TAREFA === 'Total')?'total text-white':'d2'"
+            >
+              {{ props.row.D2 }}
+            </q-td>
+
+            <q-td
+              key="d3"
+              :props="props"
+              @click="mostrar(props.row.TAREFA, 'D3')"
+              :class="(props.row.TAREFA === 'Total')?'total text-white':'d3'"
+            >
+              {{ props.row.D3 }}
+
+            </q-td>
+
+            <q-td
+              key="total"
+              :props="props"
+              @click="mostrar(props.row.TAREFA, 'TOTAL')"
+              :class="(props.row.TAREFA === 'Total')?'total text-white':'padrao'"
+            >
+              {{ props.row.TOTAL }}
+            </q-td>
+          </q-tr>
+        </template>
+      </q-table>
+      <q-table
+        v-if="mostrarDetalhamento"
+        :rows="detalhamento"
+        :columns="columnsDetalhamento"
+        row-key=""
+        table-class="col-12"
+      >
+
+      </q-table>
+      </div>
     </div>
   </q-page>
 </template>
@@ -35,7 +115,27 @@ export default {
         { name: 'd3', align: 'center', label: 'D3', field: 'D3', sortable: true, classes: 'd3' },
         { name: 'total', align: 'center', label: 'Total', field: 'TOTAL', sortable: true, classes: 'padrao' }
       ],
-      histograma: []
+      histograma: [],
+      mostrarDetalhamento: false,
+      detalhamento: [],
+      columnsDetalhamento: [
+        {
+          name: 'instancia',
+          required: true,
+          label: 'Instancia',
+          align: 'left',
+          field: row => row.INSTANCIA,
+          format: val => `${val}`,
+          sortable: true
+        },
+        { name: 'protocolo', align: 'center', label: 'Protocolo', field: 'PROTOCOLO', sortable: true },
+        { name: 'regua', align: 'center', label: 'Regua', field: 'REGUA', sortable: true },
+        { name: 'SLA', align: 'center', label: 'SLA', field: 'SLA', sortable: true },
+        { name: 'status', align: 'center', label: 'Status', field: 'STATUS', sortable: true },
+        { name: 'tipo_solicitacao', align: 'center', label: 'Tipo solicitação', field: 'TIPO_SOLICITACAO', sortable: true },
+        { name: 'inicio', align: 'center', label: 'Início', field: 'inicio', sortable: true },
+        { name: 'operador', align: 'center', label: 'Operador', field: 'operador', sortable: true }
+      ]
     }
   },
   mounted () {
@@ -51,7 +151,23 @@ export default {
         .catch((err) => {
           console.log(err)
         })
+    },
+    mostrar (linha, coluna) {
+      // this.detalhamento = linha
+      this.histograma.forEach((registro) => {
+        if (registro.TAREFA === linha) {
+          console.log('[forEach]', registro)
+          if (coluna === 'TOTAL') {
+            this.detalhamento = registro.DRILLDOWN
+          } else {
+            this.detalhamento = registro.DRILLDOWN.filter(function (obj) { return obj.REGUA === coluna })
+          }
+        }
+      })
+      this.mostrarDetalhamento = true
+      console.log(this.detalhamento)
     }
+
   }
 }
 </script>
