ac12fefc8f4d7274ac22c5818c21cd46080c6062
Gabriela Borowiak <gabriela.borowiak@catolicasc.org.br>
2023-03-15 19:11:56 -0300

    Projeto para teste iColabora

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+function Header(){
+    return (
+        <div className = "nav"> 
+            <nav className = "nav_menu">
+                <h1 className = "nav_title">  Gabriela Borowiak teste iColabora </h1>
+                   
+                
+
+            </nav>
+
+        </div>
+
+    )
+};
+
+export default Header;
